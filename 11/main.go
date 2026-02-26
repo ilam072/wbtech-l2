e374c0271710d799c967fe01740bce9276044940
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2026-02-26 16:30:55 +0500

    11

--- 11/main.go
+++ 11/main.go
@@ -7,13 +7,20 @@ import (
 )
 
 func main() {
-	fmt.Println(FindAnagrams([]string{"пятка", "пятак", "тяпка", "листок", "слиток", "столик", "стол"}))
+	fmt.Println(FindAnagrams([]string{
+		"пятка", "пятак", "тяпка",
+		"листок", "слиток", "столик",
+		"стол",
+	}))
+}
+
+type anagramGroup struct {
+	first string
+	words []string
 }
 
 func FindAnagrams(words []string) map[string][]string {
-	result := make(map[string][]string)
-	groups := make(map[string][]string)
-	firstSeen := make(map[string]string)
+	groups := make(map[string]*anagramGroup)
 
 	for _, w := range words {
 		w = strings.ToLower(w)
@@ -22,17 +29,23 @@ func FindAnagrams(words []string) map[string][]string {
 		sort.Slice(runes, func(i, j int) bool { return runes[i] < runes[j] })
 		key := string(runes)
 
-		if _, ok := firstSeen[key]; !ok {
-			firstSeen[key] = w
+		if _, ok := groups[key]; !ok {
+			groups[key] = &anagramGroup{
+				first: w,
+				words: []string{w},
+			}
+			continue
 		}
 
-		groups[key] = append(groups[key], w)
+		groups[key].words = append(groups[key].words, w)
 	}
 
-	for key, group := range groups {
-		if len(group) > 1 {
-			sort.Strings(group)
-			result[firstSeen[key]] = group
+	result := make(map[string][]string)
+
+	for _, group := range groups {
+		if len(group.words) > 1 {
+			sort.Strings(group.words)
+			result[group.first] = group.words
 		}
 	}
 
